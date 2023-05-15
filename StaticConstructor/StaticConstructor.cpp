030b1c90d03881b694a954e67de7a6e6ec5acf59
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2023-05-15 15:26:36 +0700

    membuat class mahasiswa

--- StaticConstructor/StaticConstructor.cpp
+++ StaticConstructor/StaticConstructor.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 #include <string>
+using namespace std;
+
+class mahasiswa {
+public:
+    static int nim;
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();
+    mahasiswa(string pnama) :nama(pnama) { setID(); }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
