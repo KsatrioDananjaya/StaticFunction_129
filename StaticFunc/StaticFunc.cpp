375e71ebda38031a32e3494f19d5438ea9884042
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2023-05-15 15:56:09 +0700

    memasukan main

--- StaticFunc/StaticFunc.cpp
+++ StaticFunc/StaticFunc.cpp
@@ -30,8 +30,13 @@ void mahasiswa::printAll() {
     cout << endl;
 }
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    mahasiswa mhs1("Sri Dadi");
+    mahasiswa mhs2("Budi Jatmiko");
+    mahasiswa::setNim(9); //mengakses nim melalui static function
+    mahasiswa mhs3("Andi Janu");
+    mahasiswa mhs4("Joko Wahono");
+
+
 }
 
